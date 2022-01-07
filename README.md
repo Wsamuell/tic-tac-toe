d3bfa24d2b2e743ccfe61a04729adab3317848f6
samuel Odubamowo <wemsamuel@gmail.com>
2022-01-07 13:01:20 -0700

    board implementation

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ My most helpful resources
 [Haskel Cheat sheet](https://www.newthinktank.com/2015/08/learn-haskell-one-video/)
 
 [Youtube haskell tutorial for beginner](https://www.youtube.com/watch?v=02_H3LjqMr8)
+
+https://www.tutorialspoint.com/haskell/haskell_functor.htm
+
+[Understanding how deriving works along with how to use EQ & Show](https://www.seas.upenn.edu/~cis552/13fa/lectures/Classes.html)
+[Similar Tic Tac Toe Game ](https://dev.to/nt591/writing-a-tictactoe-game-in-haskell-545e) - I used a very similar strategy to come up with how the board was going to be created and implemented.
--- index.hs
+++ index.hs
@@ -1,6 +1,5 @@
-import Data.List ()
+import Data.List (transpose, intercalate, intersperse)
 import System.IO ()
-
 
 -- to run ghci compiler you can do ghci and that compiles all changes then do :l {filename} index.hs. this gets you back to main. :r for reload
 -- :t gets the type declaration for what ever you typed in
@@ -12,41 +11,89 @@ import System.IO ()
 
 
 -- Steps to creating the game
+-- welcome  to tic tac toe
+welcomeMessage :: String -> IO ()
+welcomeMessage message = do
+    putStrLn "TIC TAC TOE"
+    putStrLn "TIC TAC TOE"
+    putStrLn "TIC TAC TOE"
+    where 
+
 
 
 -- create the pieces
 data Piece = X | O 
-instance Show Piece where
-    show X = "X"
-    show O = "O"
+    deriving Show
+
+-- create the Board 
 
 
--- create the Board
+-- image of board 
+{-
+ O | 2 | 3
+---+---+---
+ 4 | X | 6 
+---+---+---
+ 7 | 8 | 9
+-}
 data Spot = Used Piece | Unused
 instance Show Spot where
     show (Used X) = "X"
     show (Used O) = "O"
-    show Unused = ""
+    show Unused = " "
 
+instance Eq Spot where
+    Used X == Used X = True 
+    Used O == Used O = True
+    Unused == Unused = True
+    _ == _ = False
 
-
+data GamePlay = Game {
+    gameBoard :: [Spot],
+    playerTurn :: Piece
+} deriving Show
 
 data Player = Player {
     name :: String,
     character :: String,
     wins :: Int
     -- deriving is used to generate instance(show, eq)
-} deriving(Show)
+} deriving Show
+
+boardBlock ::  [Spot] -> String
+boardBlock block = intercalate "  | " $ map show block
+
+divider :: String 
+divider = "---+----+---"
+
+renderBoard :: [Spot] -> IO()
+renderBoard spots = do
+    putStrLn $ boardBlock block1
+    putStrLn divider
+    putStrLn $ boardBlock block2
+    putStrLn divider
+    putStrLn $ boardBlock block3
+    where block1 = take 3 spots
+          block2 = drop 3 . take 6 $ spots  
+          block3 = drop 6 spots
+
+-- newGame :: GamePlay
+newGame = do
+    renderBoard [Unused, Unused, Unused, Unused, Unused, Unused, Unused, Unused, Unused]
+-- i believe the best implementation of this from my end would be to use the take 3 method in haskell and use it to make an array pf an array fo that way we can have 9 number  in 3 different arrays like 
+-- [1,2..9]
+-- and then take 3 (something like this)
 
 
+-- add player piece to the board
 
--- align piece and board together so we can tell which spot is used and unused
+
 -- player switch implementation
 -- player turns, switch player on input submit.
 -- make combo of winning situations
 -- before submitting the input we should check if last player turn wins, if not  check if there are open spots to play
 -- if there is a winning combo, assign winner
--- Display winner and total wins for the duration of possible (probably not without local storage)
+-- Display winner and total wins for the duration of possible
 -- ask if users would like to restart the game
 
 -- main function in with we will call the other data and also play the game in
@@ -60,5 +107,3 @@ data Player = Player {
 
 
 -- type declaration to start the game
-startGame :: [Spot] -> Player -> IO ()
-startGame = undefined 
