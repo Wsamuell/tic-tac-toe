fcc9c0d1743ce124192416e60b6b6b0a9a9a2f99
samuel Odubamowo <wemsamuel@gmail.com>
2022-01-08 12:55:56 -0700

    updatec welcome message and restart message. addMove method is also slowly being added, still need to figure out how to get the index of each element in the array in haskel and check if they have been used

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Tic-tac-toe game designed with CLI &amp; Haskell
 
-### Haskell for Beginner tips i learned
+### Haskell for Beginners tips i learned
 
 - to run ghci compiler you can do ghci and that compiles all changes then do :l {filename}. this gets you back to main.
 
@@ -13,11 +13,14 @@ Tic-tac-toe game designed with CLI &amp; Haskell
 
 - :q to quit compiler
 
-My most helpful resources
+### My most helpful resources
+
 [Haskell Cheat sheet](https://www.newthinktank.com/2015/08/learn-haskell-one-video/)
 
 [Youtube haskell tutorial for beginner](https://www.youtube.com/watch?v=02_H3LjqMr8)
 
-https://www.tutorialspoint.com/haskell/haskell_functor.htm
+[Learning about functors for use of map and fmap implementation](https://www.tutorialspoint.com/haskell/haskell_functor.htm)
 
 [Understanding how deriving works along with how to use EQ & Show](https://www.seas.upenn.edu/~cis552/13fa/lectures/Classes.html)
+
+[Haskell Operators and Lexical Notation](https://imada.sdu.dk/~rolf/Edu/DM22/F06/haskell-operatorer.pdf)
--- index.hs
+++ index.hs
@@ -1,7 +1,26 @@
 import Data.List (transpose, intercalate, intersperse)
 import System.IO ()
-
+import Data.Maybe
 -- Steps to creating the game
+
+-- Welcome message to users
+welcomeInfo :: IO ()
+welcomeInfo = do
+    putStrLn $ replicate 8 '\n'
+    putStrLn "Welcome to my TIC-TAC-TOE game"
+    putStrLn "This is a two person local multiplayer game"
+    putStrLn "To enter your move please type in a number between 1-9"
+    putStrLn $ replicate 1 '\n'
+    putStrLn " 1 | 2 | 3"
+    putStrLn "---+---+---"
+    putStrLn " 4 | 5 | 6 "
+    putStrLn "---+---+---"
+    putStrLn " 7 | 8 | 9"
+    putStrLn $ replicate 1 '\n'
+    putStrLn "Player 1 is X"
+    putStrLn "Please input your move ..."
+    putStrLn $ replicate 1 '\n'
+
 
 -- create the pieces
 data Piece = X | O 
@@ -48,17 +67,17 @@ divider = "---+----+---"
 -- render each block row and then add the divider we created in between the rows
 renderBoard :: [Spot] -> IO()
 renderBoard spots = do
-    putStrLn $ boardBlock block1
+    putStrLn $ boardBlock top
     putStrLn divider
-    putStrLn $ boardBlock block2
+    putStrLn $ boardBlock middle
     putStrLn divider
-    putStrLn $ boardBlock block3
-    where block1 = take 3 spots
-          block2 = drop 3 . take 6 $ spots  
-          block3 = drop 6 spots
+    putStrLn $ boardBlock bottom
+    where top = take 3 spots
+          middle = drop 3 . take 6 $ spots  
+          bottom = drop 6 spots
 
 -- image of board 
---     renderBoard [Used O, Unused, Unused, Unused, Used X, Unused, Unused, Unused, Unused]
+-- renderBoard [Used O, Unused, Unused, Unused, Used X, Unused, Unused, Unused, Unused]
 
 {-
  O |   |  
@@ -68,23 +87,48 @@ renderBoard spots = do
    |   |  
 -}
 
+emptyBoard :: IO ()
+emptyBoard = do
+    putStrLn $ replicate 1 '\n'
+    renderBoard [Unused, Unused, Unused, Unused, Unused, Unused, Unused, Unused, Unused]
+    putStrLn $ replicate 1 '\n'
 
 -- add player piece to the board
 
+-- so this one is a bit more tricky, the way i was thinking i would be able to do this is use the !! (findIndex) method to find unused spots on the board and then have user input the number. 
+-- if i am able to use the index, i can try to replace value in index with Used X or Used O based on input
+-- PROBLEM - I wont be able to verify if the spot is used or unused so the game logic would be compromised
+
+addMove :: Spot -> String 
+addMove = show 
 
 -- player switch implementation
--- player turns, switch player on input submit.
 -- make combo of winning situations
 -- before submitting the input we should check if last player turn wins, if not  check if there are open spots to play
 -- if there is a winning combo, assign winner
--- Display winner and total wins for the duration of possible
+
 -- ask if users would like to restart the game
+restart :: IO ()
+restart = do
+    putStrLn "Would you like to play again? (y/n)"
+    newGame <- getLine 
+    if newGame ==  "y" then do 
+        welcomeInfo
+    else if newGame == "n" then do
+        putStrLn $ replicate 8 '\n'
+        putStrLn "❤️ Thank You for Playing ❤️"
+        putStrLn $ replicate 8 '\n'
+
+        return ()
+    else do 
+        putStrLn "Input not Valid! Please try again (y/n)"
+        restart
+
 
 -- main function in with we will call the other data and also play the game in
--- main = do
---     putStrLn "Whats your name"
---     name <- getLine
---     putStrLn("hello " ++ name)
+
+
+
 
 -- IO action is a piece or pieces of code that waits to be executed like a function basically in JS
 -- main :: IO () 
