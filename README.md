4b6cea565dcc08b6eefcfcca4527605996749a5c
samuel Odubamowo <wemsamuel@gmail.com>
2022-01-07 17:52:50 -0700

    rearranged my code a bit more to reflect my thought process on board creation and Spot logic

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
-# tic-tac-toe
+# Tic-Tac-Toe
 
-Tic-tac-toe game designed with CLI &amp; Kaskell
+Tic-tac-toe game designed with CLI &amp; Haskell
+
+### Haskell for Beginner tips i learned
+
+- to run ghci compiler you can do ghci and that compiles all changes then do :l {filename}. this gets you back to main.
+
+- :r for reload
+
+- :t gets the type declaration for what ever you typed in
+- to run entire file compiler with main = do, run ghc -- make {filename}
+
+- :q to quit compiler
 
 My most helpful resources
-[Haskel Cheat sheet](https://www.newthinktank.com/2015/08/learn-haskell-one-video/)
+[Haskell Cheat sheet](https://www.newthinktank.com/2015/08/learn-haskell-one-video/)
 
 [Youtube haskell tutorial for beginner](https://www.youtube.com/watch?v=02_H3LjqMr8)
 
 https://www.tutorialspoint.com/haskell/haskell_functor.htm
 
 [Understanding how deriving works along with how to use EQ & Show](https://www.seas.upenn.edu/~cis552/13fa/lectures/Classes.html)
-[Similar Tic Tac Toe Game ](https://dev.to/nt591/writing-a-tictactoe-game-in-haskell-545e) - I used a very similar strategy to come up with how the board was going to be created and implemented.
--- index.hs
+++ index.hs
@@ -1,41 +1,12 @@
 import Data.List (transpose, intercalate, intersperse)
 import System.IO ()
 
--- to run ghci compiler you can do ghci and that compiles all changes then do :l {filename} index.hs. this gets you back to main. :r for reload
--- :t gets the type declaration for what ever you typed in
--- to run entire file compiler with main = do, run ghc -- make {filename} index.hs
-
--- :q to quit compiler
-
-
-
-
 -- Steps to creating the game
--- welcome  to tic tac toe
-welcomeMessage :: String -> IO ()
-welcomeMessage message = do
-    putStrLn "TIC TAC TOE"
-    putStrLn "TIC TAC TOE"
-    putStrLn "TIC TAC TOE"
-    where 
-
-
 
 -- create the pieces
 data Piece = X | O 
-    deriving Show
+    deriving (Show, Eq)
 
--- create the Board 
-
-
--- image of board 
-{-
- O | 2 | 3
----+---+---
- 4 | X | 6 
----+---+---
- 7 | 8 | 9
--}
 data Spot = Used Piece | Unused
 instance Show Spot where
     show (Used X) = "X"
@@ -47,6 +18,13 @@ instance Eq Spot where
     Used O == Used O = True
     Unused == Unused = True
     _ == _ = False
+
+
+-- create the Board 
+
+-- i believe the best implementation of this from my end would be to use the take 3 method in haskell and use it to make an array pf an array fo that way we can have 9 number  in 3 different arrays like 
+-- [1,2..9]
+-- and then take 3 (something like this)
 
 data GamePlay = Game {
     gameBoard :: [Spot],
@@ -60,12 +38,14 @@ data Player = Player {
     -- deriving is used to generate instance(show, eq)
 } deriving Show
 
+-- in between each line, add a separating |
 boardBlock ::  [Spot] -> String
 boardBlock block = intercalate "  | " $ map show block
 
 divider :: String 
 divider = "---+----+---"
 
+-- render each block row and then add the divider we created in between the rows
 renderBoard :: [Spot] -> IO()
 renderBoard spots = do
     putStrLn $ boardBlock block1
@@ -77,12 +57,16 @@ renderBoard spots = do
           block2 = drop 3 . take 6 $ spots  
           block3 = drop 6 spots
 
--- newGame :: GamePlay
-newGame = do
-    renderBoard [Unused, Unused, Unused, Unused, Unused, Unused, Unused, Unused, Unused]
--- i believe the best implementation of this from my end would be to use the take 3 method in haskell and use it to make an array pf an array fo that way we can have 9 number  in 3 different arrays like 
--- [1,2..9]
--- and then take 3 (something like this)
+-- image of board 
+--     renderBoard [Used O, Unused, Unused, Unused, Used X, Unused, Unused, Unused, Unused]
+
+{-
+ O |   |  
+---+---+---
+   | X |   
+---+---+---
+   |   |  
+-}
 
 
 -- add player piece to the board
