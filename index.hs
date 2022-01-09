a203df3d81d2d1f914227c67b831a77de0c1116b
samuel Odubamowo <wemsamuel@gmail.com>
2022-01-09 15:15:00 -0700

    board renders on CL and game runs well now. small issue i am having is with the tie game logic

--- index.hs
+++ index.hs
@@ -1,6 +1,6 @@
 import Data.List (intercalate)
--- import System.IO ()
 import Data.Maybe ()
+
 -- Steps to creating the game
 
 -- Welcome message to users
@@ -9,13 +9,12 @@ welcomeInfo = do
     putStrLn $ replicate 8 '\n'
     putStrLn "Welcome to my TIC-TAC-TOE game"
     putStrLn "This is a two person local multiplayer game"
-    putStrLn "To enter your move please type in a number between 1-9"
+    putStrLn "To enter your move please type in a spot between T1 - B3 (Case Sensitive)"
     putStrLn $ replicate 1 '\n'
-    putStrLn " T1 | T2 | T3"
-    putStrLn "----+----+---"
-    putStrLn " M1 | M2 | M3"
-    putStrLn "----+----+---"
-    putStrLn " B1 | B2 | B3"
+    putStrLn "T - Top"
+    putStrLn "M - Middle"
+    putStrLn "B - Bottom"
+    exampleBoard
     putStrLn $ replicate 1 '\n'
     putStrLn "Player 1 is X"
     putStrLn "Please input your move ..."
@@ -50,7 +49,7 @@ data GamePlay = Game {
     playerTurn :: Piece
 } deriving Show
 
--- User data implementation can be added as needed
+-- User data implementation can be added as needed. maybe for later versions of the game
 
 data PlayerMove = Valid [Spot] | Invalid String [Spot]
 -- data Player = Player {
@@ -91,10 +90,14 @@ renderBoard spots = do
 -}
 
 -- this is just an image you want the Users to get an idea of what n empty game board with free spaces look like 
-emptyBoard :: IO ()
-emptyBoard = do
+exampleBoard :: IO ()
+exampleBoard = do
     putStrLn $ replicate 1 '\n'
-    renderBoard [Unused, Unused, Unused, Unused, Unused, Unused, Unused, Unused, Unused]
+    putStrLn " T1 | T2 | T3"
+    putStrLn "----+----+---"
+    putStrLn " M1 | M2 | M3"
+    putStrLn "----+----+---"
+    putStrLn " B1 | B2 | B3"
     putStrLn $ replicate 1 '\n'
 
 -- add player piece to the board
@@ -103,6 +106,7 @@ emptyBoard = do
 -- if i am able to use the index, i can try to replace value in index with Used X or Used O based on input
 -- PROBLEM - I wont be able to verify if the spot is used or unused so the game logic would be compromised
 
+-- the above steps didnt work so i looked into seeing how else i can  implement piece rendering code. after much review i watched a couple of videos and read haskell code which i have linked in the README and decided i can just make a list of index based on the pieces and spots available 
 
 -- check if the current spot a player takes is valid or invalid
 
@@ -131,7 +135,7 @@ assignValidSpot :: String -> Piece -> [Spot] -> PlayerMove
 assignValidSpot userInput piece spot = 
     case spotIndex userInput >>= spotIsValid spot of
         Nothing -> Invalid "Player Move Invalid... Try Again" spot
-        Just i -> Valid (take i spot ++ [Used piece] ++ drop (i + 1) spot)
+        Just index -> Valid (take index spot ++ [Used piece] ++ drop (index + 1) spot)
 
 
 -- player switch implementation
@@ -142,26 +146,42 @@ switchPlayer X = O
 -- Now we need to ask for user input and then assign Spot as requested
 newGame :: Piece -> [Spot] -> IO()
 newGame  piece spot = do 
-    putStrLn ("Player " ++ show piece ++ "'s turn")
-    renderBoard spot
-    putStr( "\nPlayer " ++ show piece ++ "'s turn: ")
+    putStrLn ("Player " ++ show piece ++ " please make a move")
+    exampleBoard
+    putStr( "\nPlayer " ++ show piece ++ "'s turn (CASE SENSITIVE): ")
     myMove <- getLine
     case assignValidSpot myMove piece spot of
+
+-- App is now fully functional but i have an issue where user input is not displayed on the board but registered to assigned spot on board. 
+-- i need to find a way to have renderBoard display the move i played and then make sure the spot is removed from the board so it cant be used twice
+
         Invalid err spot -> do
             putStrLn err
             newGame piece spot
 -- before submitting the input we should check if last player turn wins, if not  check if there are open spots to play
-        Valid updateBoard -> do
-            if checkWinner piece updateBoard then do
--- if there is a winning combo, assign winner
-                putStrLn ("Player" ++ show piece ++ "Wins the Game!" )
-                renderBoard spot
+        Valid gameBoard -> do
+-- if there are no spots left, render tied game and restart 
+            if checkTie piece gameBoard then do 
+                putStrLn "Tied Game"
                 restart
-            else newGame (switchPlayer piece) spot
+            else if checkWinner piece gameBoard then do
+-- if there is a winning combo, assign winner
+                putStrLn ("Player " ++ show piece ++ " Wins the Game!" )
+                putStrLn $ replicate 1 '\n'
+                renderBoard gameBoard
+                restart
+            else do
+                renderBoard gameBoard
+                newGame (switchPlayer piece) gameBoard
 
+-- check tie game. checks if all the spots are filled with used 
+checkTie :: Piece -> [Spot] -> Bool 
+checkTie piece spot = 
+    or [
+        head spot == Used piece && spot !! 1 == Used piece && spot !! 2 == Used piece && spot !! 3 == Used piece && spot !! 4 == Used piece && spot !! 5 == Used piece && spot !! 6 == Used piece && spot !! 7 == Used piece && spot !! 8 == Used piece
+    ]
 
 -- make combo of winning situations
-
 checkWinner :: Piece -> [Spot] -> Bool 
 checkWinner piece spot =  
     -- or checks if any of the conditions in the array is true for the boolean defined
@@ -177,7 +197,7 @@ checkWinner piece spot =
         -- winning situation for middle column
         spot !! 1 == Used piece && spot !! 4 == Used piece && spot !! 7 == Used piece,
         -- winning situation for right column
-        spot !! 2 == Used piece && spot !! 3 == Used piece && spot !! 8 == Used piece,
+        spot !! 2 == Used piece && spot !! 5 == Used piece && spot !! 8 == Used piece,
         -- winning situation for \
         head spot == Used piece && spot !! 4 == Used piece && spot !! 8 == Used piece,
         -- winning situation for /
@@ -215,5 +235,5 @@ restart = do
 main :: IO()
 main = do 
     welcomeInfo
-    let newBoard = replicate 9 Unused 
-    newGame X newBoard
+    let gameBoard = replicate 9 Unused 
+    newGame X gameBoard
