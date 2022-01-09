f0b4c4c1b7cdcd90a812f0f6a2bc8ff0daf2dea1
samuel Odubamowo <wemsamuel@gmail.com>
2022-01-08 19:02:32 -0700

    user is not able to make moves but it doesnt render on the board thats the next logic needed to be completed

--- README.md
+++ README.md
@@ -24,3 +24,7 @@ Tic-tac-toe game designed with CLI &amp; Haskell
 [Understanding how deriving works along with how to use EQ & Show](https://www.seas.upenn.edu/~cis552/13fa/lectures/Classes.html)
 
 [Haskell Operators and Lexical Notation](https://imada.sdu.dk/~rolf/Edu/DM22/F06/haskell-operatorer.pdf)
+
+### Haskell Tic Tac Toe Resources
+
+[Making tic tac toe in Haskell tutorial](https://www.youtube.com/watch?v=0-pOaa0dpko&t=149s) - I found this Resource very confusing when i first started developing my game but i wanted to get a rough idea of how i want to implement my code even before i start Pseudo-coding. i watched half the tutorial and decided it wasn't the path for i was trying to implement
--- index.hs
+++ index.hs
@@ -1,6 +1,6 @@
-import Data.List (transpose, intercalate, intersperse)
-import System.IO ()
-import Data.Maybe
+import Data.List (intercalate)
+-- import System.IO ()
+import Data.Maybe ()
 -- Steps to creating the game
 
 -- Welcome message to users
@@ -11,11 +11,11 @@ welcomeInfo = do
     putStrLn "This is a two person local multiplayer game"
     putStrLn "To enter your move please type in a number between 1-9"
     putStrLn $ replicate 1 '\n'
-    putStrLn " 1 | 2 | 3"
-    putStrLn "---+---+---"
-    putStrLn " 4 | 5 | 6 "
-    putStrLn "---+---+---"
-    putStrLn " 7 | 8 | 9"
+    putStrLn " T1 | T2 | T3"
+    putStrLn "----+----+---"
+    putStrLn " M1 | M2 | M3"
+    putStrLn "----+----+---"
+    putStrLn " B1 | B2 | B3"
     putStrLn $ replicate 1 '\n'
     putStrLn "Player 1 is X"
     putStrLn "Please input your move ..."
@@ -50,12 +50,15 @@ data GamePlay = Game {
     playerTurn :: Piece
 } deriving Show
 
-data Player = Player {
-    name :: String,
-    character :: String,
-    wins :: Int
+-- User data implementation can be added as needed
+
+data PlayerMove = Valid [Spot] | Invalid String [Spot]
+-- data Player = Player {
+--     name :: String,
+--     character :: String,
+--     wins :: Int
     -- deriving is used to generate instance(show, eq)
-} deriving Show
+-- } deriving Show
 
 -- in between each line, add a separating |
 boardBlock ::  [Spot] -> String
@@ -87,6 +90,7 @@ renderBoard spots = do
    |   |  
 -}
 
+-- this is just an image you want the Users to get an idea of what n empty game board with free spaces look like 
 emptyBoard :: IO ()
 emptyBoard = do
     putStrLn $ replicate 1 '\n'
@@ -99,13 +103,86 @@ emptyBoard = do
 -- if i am able to use the index, i can try to replace value in index with Used X or Used O based on input
 -- PROBLEM - I wont be able to verify if the spot is used or unused so the game logic would be compromised
 
-addMove :: Spot -> String 
-addMove = show 
+
+-- check if the current spot a player takes is valid or invalid
+
+spotIsValid :: [Spot] -> Int -> Maybe Int
+spotIsValid  spots ix  = if spots !! ix == Unused then Just ix else Nothing
+
+-- options for checking position on created board
+-- T - top
+-- M - middle
+-- B- bottom
+spotIndex :: String -> Maybe Int 
+spotIndex "T1" = Just 0
+spotIndex "T2" = Just 1
+spotIndex "T3" = Just 2
+spotIndex "M1" = Just 3
+spotIndex "M2" = Just 4
+spotIndex "M3" = Just 5
+spotIndex "B1" = Just 6
+spotIndex "B2" = Just 7
+spotIndex "B3" = Just 8
+spotIndex _ = Nothing 
+
+
+-- if its valid then assign the spot to the current player
+assignValidSpot :: String -> Piece -> [Spot] -> PlayerMove
+assignValidSpot userInput piece spot = 
+    case spotIndex userInput >>= spotIsValid spot of
+        Nothing -> Invalid "Player Move Invalid... Try Again" spot
+        Just i -> Valid (take i spot ++ [Used piece] ++ drop (i + 1) spot)
+
 
 -- player switch implementation
--- make combo of winning situations
+switchPlayer :: Piece -> Piece
+switchPlayer O = X
+switchPlayer X = O
+
+-- Now we need to ask for user input and then assign Spot as requested
+newGame :: Piece -> [Spot] -> IO()
+newGame  piece spot = do 
+    putStrLn ("Player " ++ show piece ++ "'s turn")
+    renderBoard spot
+    putStr( "\nPlayer " ++ show piece ++ "'s turn: ")
+    myMove <- getLine
+    case assignValidSpot myMove piece spot of
+        Invalid err spot -> do
+            putStrLn err
+            newGame piece spot
 -- before submitting the input we should check if last player turn wins, if not  check if there are open spots to play
+        Valid updateBoard -> do
+            if checkWinner piece updateBoard then do
 -- if there is a winning combo, assign winner
+                putStrLn ("Player" ++ show piece ++ "Wins the Game!" )
+                renderBoard spot
+                restart
+            else newGame (switchPlayer piece) spot
+
+
+-- make combo of winning situations
+
+checkWinner :: Piece -> [Spot] -> Bool 
+checkWinner piece spot =  
+    -- or checks if any of the conditions in the array is true for the boolean defined
+    or [
+        -- winning situation for top row
+        head spot == Used piece && spot !! 1 == Used piece && spot !! 2 == Used piece,
+        -- winning situation for middle row
+        spot !! 3 == Used piece && spot !! 4 == Used piece && spot !! 5 == Used piece,
+        -- winning situation for bottom row
+        spot !! 6 == Used piece && spot !! 7 == Used piece && spot !! 8 == Used piece,
+        -- winning situation for left column
+        head spot == Used piece && spot !! 3 == Used piece && spot !! 6 == Used piece,
+        -- winning situation for middle column
+        spot !! 1 == Used piece && spot !! 4 == Used piece && spot !! 7 == Used piece,
+        -- winning situation for right column
+        spot !! 2 == Used piece && spot !! 3 == Used piece && spot !! 8 == Used piece,
+        -- winning situation for \
+        head spot == Used piece && spot !! 4 == Used piece && spot !! 8 == Used piece,
+        -- winning situation for /
+        spot !! 2 == Used piece && spot !! 4 == Used piece && spot !! 6 == Used piece
+    ]
 
 -- ask if users would like to restart the game
 restart :: IO ()
@@ -113,7 +190,7 @@ restart = do
     putStrLn "Would you like to play again? (y/n)"
     newGame <- getLine 
     if newGame ==  "y" then do 
-        welcomeInfo
+        main
     else if newGame == "n" then do
         putStrLn $ replicate 8 '\n'
         putStrLn "❤️ Thank You for Playing ❤️"
@@ -135,3 +212,8 @@ restart = do
 
 
 -- type declaration to start the game
+main :: IO()
+main = do 
+    welcomeInfo
+    let newBoard = replicate 9 Unused 
+    newGame X newBoard
